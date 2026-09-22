5b7ab52ba0f2869c417b8585fe0d7840f3f6b1d2
Waibern <waiberny12@gmail.com>
2026-09-22 10:36:05 +0700

    Run Vercel API entry as native ESM

--- api/index.js
+++ api/index.js
@@ -1,9 +1,4 @@
-// Vercel serverless entry point. The API folder is loaded as CommonJS by
-// Vercel, while the Express server uses native ESM.
-let appPromise;
+// Vercel serverless entry point. The repository uses native ESM throughout.
+import app from '../server/server.js';
 
-module.exports = async (req, res) => {
-  appPromise ||= import('../server/server.js').then(module => module.default);
-  const app = await appPromise;
-  return app(req, res);
-};
+export default app;
