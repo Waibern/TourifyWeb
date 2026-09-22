964c47820bf3d2c03ebbbe227598e22176622e5c
Waibern <waiberny12@gmail.com>
2026-09-22 10:27:33 +0700

    Fix Vercel ESM API entrypoint

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
-// Vercel serverless entry point. Express owns all /api/* routes.
-import app from '../server/server.js'
-export default app
+// Vercel serverless entry point. The API folder is loaded as CommonJS by
+// Vercel, while the Express server uses native ESM.
+let appPromise;
+
+module.exports = async (req, res) => {
+  appPromise ||= import('../server/server.js').then(module => module.default);
+  const app = await appPromise;
+  return app(req, res);
+};
